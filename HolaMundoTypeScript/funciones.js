a22c6c38a11324a1fc42c6eccd15ff8e51fc9f8a
vertildo <vertildo@rus.com>
2023-01-18 10:32:00 -0300

    chore: first commit

---
+++ HolaMundoTypeScript/funciones.js
@@ -0,0 +1,15 @@
+"use strict";
+var saludo = "HolaMundo desde TypeScript";
+saludo = "cambio";
+var numero;
+numero = 10;
+var cualquiera;
+cualquiera = "cadena";
+cualquiera = 10;
+var PI_NUMERO = 3.14;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+    console.log(PI_NUMERO);
+}
+saludar();
